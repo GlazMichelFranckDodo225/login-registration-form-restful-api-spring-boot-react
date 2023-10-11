1489b013900bdf3c57824a3ccb83483572f1a22f
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-11 04:35:01 +0200

    Backend : Creation of all packages and the "EmployeeController" Class

---
+++ backend-spring-boot/src/main/java/com/dgmf/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.dgmf.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+}
